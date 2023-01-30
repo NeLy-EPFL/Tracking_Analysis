ff47b52172796a73cd930c023c5feb43a6b0cfe3
MatthiasDurrieu <matthias.durrieu@epfl.ch>
2023-01-30 11:27:38 +0100

    Implemented almost fully automatic ball tracking processing pipeline. Added bootstrapped confidence interval to processing functions.

--- Utilities/Processing.py
+++ Utilities/Processing.py
@@ -1,4 +1,7 @@
 from scipy import signal
+import numpy as np
+
+# Compute low-pass filtered data
 
 def butter_lowpass_filter(
     data,
@@ -10,3 +13,26 @@ def butter_lowpass_filter(
     b, a = signal.butter(order, cutoff, btype="low", analog=False)
     y = signal.filtfilt(b, a, data)
     return y
+
+# Compute bootstrapped confidence interval
+
+rg = np.random.default_rng()
+
+
+def draw_bs_rep(data, func, rg):
+    """Compute a bootstrap replicate from data."""
+    bs_sample = rg.choice(data, size=len(data))
+    return func(bs_sample)
+
+
+def draw_bs_ci(data, func=np.mean, rg=rg, n_reps=2000):
+    """Sample bootstrap multiple times and compute confidence interval
+        arguments:
+            data: array-like
+            func: function to compute confidence interval
+            rg: random generator
+            n_reps: number of bootstrap replicates
+    """
+    bs_reps = np.array([draw_bs_rep(data, func, rg) for _ in range(n_reps)])
+    conf_int = np.percentile(bs_reps, [2.5, 97.5])
+    return conf_int
