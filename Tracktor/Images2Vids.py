21f1541cf744e36c6618abcd1184e3624e6abf33
matthias <matthiasdurrieu@gmail.com>
2023-06-07 09:55:21 +0200

    Implemented separate image2vid function Implemented video subsetting before processing

---
+++ Tracktor/Images2Vids.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+folder_path = Path("/home/matthias/Videos/230606_DarkishTest_Cropped_Subsets/")
+output_folder = folder_path.parent / f"{folder_path.stem}_Videos"
+
+def create_video_from_images(images_folder, output_folder, video_name, fps):
+    f = images_folder.as_posix()
+    video_path = f"{output_folder.as_posix()}/{video_name}.mp4"
+    if not os.path.exists(video_path):
+        terminal_call = f"ffmpeg -loglevel panic -nostats -hwaccel cuda -r {fps} -i {f}/image%d_cropped.jpg -pix_fmt yuv420p -c:v libx265 -x265-params log-level=error -vsync 0 -crf 15 {video_path}"
+        os.system(terminal_call)
+        return True
+    else:
+        return False
+
+def search_folder_for_images(folder_path, output_folder, fps):
+    subdirs = []
+    for subdir, dirs, files in os.walk(folder_path):
+        if any(file.endswith('.jpg') for file in files):
+            subdirs.append(subdir)
+    with tqdm(total=len(subdirs), desc="Processing videos") as pbar:
+        for subdir in subdirs:
+            video_name = '_'.join(Path(subdir).parts[-2:])
+            create_video_from_images(Path(subdir), output_folder, video_name, fps)
+            pbar.update(1)
+
+fps = "30"
+search_folder_for_images(folder_path, output_folder, fps)
