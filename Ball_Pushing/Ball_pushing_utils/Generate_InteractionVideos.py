45efb9a4f41bab71fd2146b3877ba7d0f82fba92
matthias <matthiasdurrieu@gmail.com>
2024-03-05 09:51:53 +0100

    Updated interaction videos script to prevent reprocessing videos twice when running the code again

--- Ball_Pushing/Ball_pushing_utils/Generate_InteractionVideos.py
+++ Ball_Pushing/Ball_pushing_utils/Generate_InteractionVideos.py
@@ -6,42 +6,42 @@ sys.path.insert(0, "../..")
 
 sys.path.insert(0, "../../Utilities")
 
-from Utils import *
-from Ballpushing_utils import *
+import Utils
+import Ballpushing_utils
 
-# # Get the data path
-# datapath = get_data_path()
+# Get the data path
+datapath = Utils.get_data_path()
 
-# print(f"Data path: {datapath}")
+print(f"Data path: {datapath}")
 
-# # Generate a list of experiments to consider
+# Generate a list of experiments to consider
 
 
-# Folders = []
-# for folder in datapath.iterdir():
-#     minfolder = str(folder).lower()
-#     if "tnt_fine" in minfolder and "tracked" in minfolder:
-#         Folders.append(folder)
+Folders = []
+for folder in datapath.iterdir():
+    minfolder = str(folder).lower()
+    if "tnt_fine" in minfolder and "tracked" in minfolder:
+        Folders.append(folder)
 
-# print(Folders)
+print(Folders)
 
-# # For each folder, generate an Experiment object
-# Experiments = []
-# for folder in Folders:
-#     Experiments.append(Experiment(folder))
+# For each folder, generate an Experiment object
+Experiments = []
+for folder in Folders:
+    Experiments.append(Ballpushing_utils.Experiment(folder))
 
-# Load the experiments from a file
+# # Load the experiments from a file
 
-loadpath = (
-    get_labserver()
-    / "Experimental_data/MultiMazeRecorder/Datasets/240129_TNT_Fine_Experiments.pkl"
-)
+# # loadpath = (
+# #     get_labserver()
+# #     / "Experimental_data/MultiMazeRecorder/Datasets/240129_TNT_Fine_Experiments.pkl"
+# # )
 
-Experiments = load_object(loadpath.as_posix())
+# #Experiments = load_object(loadpath.as_posix())
 
 # For each fly in each experiment, generate the interaction video and save it in a folder (which might need to be created) depending on its genotype
 
-savepath = get_labserver() / "Videos" / "240129_TNT_Fine"
+savepath = Utils.get_labserver() / "Videos" / "240129_TNT_Fine"
 
 # Create the folder if it doesn't exist
 if not savepath.exists():
@@ -50,6 +50,12 @@ if not savepath.exists():
 for experiment in Experiments:
     for fly in experiment.flies:
         # Check if the folder for the genotype exists
-        if not (savepath / fly.Genotype).exists():
-            (savepath / fly.Genotype).mkdir()
-        fly.generate_interactions_video(outpath=savepath / fly.Genotype)
+        genotype_path = savepath / fly.Genotype
+        if not genotype_path.exists():
+            genotype_path.mkdir()
+        # Check if the video already exists
+        vidname = f"{fly.name}_{fly.Genotype if fly.Genotype else 'undefined'}.mp4"
+        video_path = genotype_path / vidname
+        if not video_path.exists():
+            # If the video doesn't exist, generate it
+            fly.generate_interactions_video(outpath=genotype_path)
