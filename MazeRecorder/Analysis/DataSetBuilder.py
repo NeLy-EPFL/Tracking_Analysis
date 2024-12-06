9da2e79ff51df8eed0a322de0eeb8c5a805ad3fd
durrieu <matthiasdurrieu@gmail.com>
2024-12-06 15:04:50 +0100

    Added contact based coordinates data in the datasetbuilder

--- MazeRecorder/Analysis/DataSetBuilder.py
+++ MazeRecorder/Analysis/DataSetBuilder.py
@@ -39,56 +39,91 @@ import re
 
 data_path = Utils.get_data_path()
 
-savepath = Path("/mnt/upramdya_data/MD/MultiMazeRecorder/Datasets/Skeleton_TNT/Experiments")
+experiment_path = Path("/mnt/upramdya_data/MD/MultiMazeRecorder/Datasets/Skeleton_TNT/Experiments")
 
-tnt_folders = [folder for folder in data_path.iterdir() if folder.is_dir() and 'TNT_Fine' in folder.name]
+savepath = Path("/mnt/upramdya_data/MD/MultiMazeRecorder/Datasets/Skeleton_TNT/Contact_data")
 
-print (f" Folders to analyse : {tnt_folders}")
+#tnt_folders = [folder for folder in data_path.iterdir() if folder.is_dir() and 'TNT_Fine' in folder.name]
 
+#print(f" Folders to analyse : {tnt_folders}")
 
 # For each folder, generate the experiment object and then make a dataset out of it, then save it to csv
 
-for folder in tnt_folders:
-    # Check if a dataset with the same name already exists :
+# for folder in tnt_folders:
+#     # Check if a dataset with the same name already exists :
     
-    dataset_name = folder.name + ".csv"
+#     dataset_name = folder.name + ".csv"
     
-    # If it does, skip the folder
-    # if (savepath/dataset_name).exists():
-    #     print(f"Dataset {dataset_name} already exists. Skipping folder {folder.name}")
-    #     continue
+#     # If it does, skip the folder
+#     # if (savepath/dataset_name).exists():
+#     #     print(f"Dataset {dataset_name} already exists. Skipping folder {folder.name}")
+#     #     continue
 
-    if (savepath/f"/Experiments/{folder.name}.pkl").exists():
-        print(f"Experiment {folder.name} already exists. Skipping folder {folder.name}")
-        continue
+#     if (savepath / f"{folder.name}.pkl").exists():
+#         print(f"Experiment {folder.name} already exists. Skipping folder {folder.name}")
+#         continue
     
-    experiment = Ballpushing_utils.Experiment(folder)
+#     experiment = Ballpushing_utils.Experiment(folder)
     
-    try:
-        Ballpushing_utils.save_object(experiment, savepath/f"/Experiments/{folder.name}.pkl")
-    except Exception as e:
-        print(f"Could not save experiment {folder.name}")
-        print(e)
-        
-        continue
+#     try:
+#         Ballpushing_utils.save_object(experiment, savepath / f"{folder.name}.pkl")
+#     except Exception as e:
+#         print(f"Could not save experiment {folder.name}")
+#         print(e)
+#         continue
     
-    dataset = Ballpushing_utils.Dataset(experiment)
+#     dataset = Ballpushing_utils.Dataset(experiment)
     
-    try:
-        dataset.generate_dataset(metrics = "Skeleton_contacts")
-        
-        dataset.data.to_csv(savepath/dataset_name)
-        
-        print(f"Dataset {dataset_name} saved.")
-        
-    except:
-        print(f"Could not generate dataset for {folder.name}")
-        continue
+#     try:
+#         dataset.generate_dataset(metrics="Skeleton_contacts")
+#         dataset.data.to_csv(savepath / dataset_name)
+#         print(f"Dataset {dataset_name} saved.")
+#     except Exception as e:
+#         print(f"Could not generate dataset for {folder.name}")
+#         print(e)
+#         continue
 
-# Then, concatenate all the datasets into one big dataset
+# # Then, concatenate all the datasets into one big dataset
 
 # datasets = [pd.read_csv(file) for file in savepath.iterdir() if file.suffix == ".csv"]
 
 # big_dataset = pd.concat(datasets)
 
 # big_dataset.to_csv(savepath/"241204_Pooled.csv")
+
+Exps = experiment_path.glob("*.pkl")
+
+for experiment_path in Exps:
+    print(f"loading {experiment_path}")
+    
+    dataset_path = savepath / f"{experiment_path.stem}_contact_data.csv"
+    
+    if dataset_path.exists():
+        print(f"Dataset {dataset_path} already exists. Skipping experiment {experiment_path.stem}")
+        continue
+    
+    try:
+        experiment = Ballpushing_utils.load_object(experiment_path)
+    except Exception as e:
+        print(f"Could not load experiment {experiment_path}")
+        print(e)
+        continue
+    
+    try:
+        dataset = Ballpushing_utils.Dataset(experiment, dataset_type="contact_data")
+        dataset.data.to_csv(dataset_path)
+        print(f"Dataset {dataset_path} saved.")
+    except Exception as e:
+        print(f"Could not generate dataset for {experiment_path.stem}")
+        print(e)
+        continue
+    
+
+# Then, concatenate all the datasets into one big dataset
+
+datasets = [pd.read_csv(file) for file in savepath.iterdir() if file.suffix == ".csv"]
+
+big_dataset = pd.concat(datasets)
+
+big_dataset.to_csv(savepath/"241206_Pooled_contact_data.csv")
+    
