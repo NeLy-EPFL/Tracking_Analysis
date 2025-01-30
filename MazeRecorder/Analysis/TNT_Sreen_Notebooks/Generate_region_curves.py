c0a004e3478d0c1101f94d80f4f88b592a933d14
durrieu <matthiasdurrieu@gmail.com>
2025-01-30 11:57:32 +0100

    Improved region wise curves plotting to avoid plotting irrelevant lines

--- MazeRecorder/Analysis/TNT_Sreen_Notebooks/Generate_region_curves.py
+++ MazeRecorder/Analysis/TNT_Sreen_Notebooks/Generate_region_curves.py
@@ -44,45 +44,60 @@ import importlib
 
 registries = Config.registries
 data_path = Path("/mnt/upramdya_data/MD/MultiMazeRecorder/Datasets/Skeleton_TNT/240120_short_contacts_no_cutoff_no_downsample_Data/coordinates_regions")
+output_dir = Path("/mnt/upramdya_data/MD/MultiMazeRecorder/Plots/250130_Coordinates_Full_Signif")
+
+
 miscellaneous_nicknames = []
-# Add a progress bar to the brain regions processing loop
-for brain_region in tqdm(registries["brain_regions"], desc="Processing brain regions"):
-    output_path = f"/mnt/upramdya_data/MD/MultiMazeRecorder/Plots/250122_Coordinates_full/Full_euclidean_distance_coordinates_line_{brain_region}.png"
+for brain_region in registries["brain_regions"]:
     
-    if os.path.exists(output_path):
-        print(f"Skipping Brain region {brain_region} as the plot already exists.")
-        continue
+    if brain_region != "Control":
+        output_path = output_dir / f"Full_euclidean_distance_coordinates_line_{brain_region}.png"
     
-    print(f"Processing Brain region {brain_region}")
-    print("loading datasets")
-    region_data = Config.load_datasets_for_brain_region(brain_region, data_path, registries, downsample_factor=None)
-    nicknames = region_data['Nickname'].unique()
-    
-    if len(nicknames) < 5 and brain_region != "Control":
-        miscellaneous_nicknames.extend(nicknames)
-        continue
-    
-    try:
-        print("Creating and saving plot")
-        Config.create_and_save_plot(region_data, nicknames, brain_region, output_path, registries, show_progress=True)
-        print(f"Processed Brain region {brain_region}")
-    except Exception as e:
-        print(f"Error processing Brain region {brain_region}: {e}")
+        if os.path.exists(output_path):
+            print(f"Skipping Brain region {brain_region} as the plot already exists.")
+            continue
+        
+        BallTrajectories = Config.load_datasets_for_brain_region(brain_region, data_path, registries, downsample_factor=None)
+        
+        region_data = BallTrajectories[BallTrajectories['Brain region'] != "Control"]
+        nicknames = region_data['Nickname'].unique()
+        
+        if len(nicknames) < 5 and brain_region != "Control":
+            miscellaneous_nicknames.extend(nicknames)
+            continue
+        try:
+            Config.create_and_save_plot(region_data, nicknames, brain_region, output_path, registries, show_signif=True)
+            print(f"Processed Brain region {brain_region}")
+        except Exception as e:
+            print(f"Error processing Brain region {brain_region}: {e}")
+            
+    else:
+        output_path = output_dir / "Full_euclidean_distance_coordinates_line_Control.png"
+        
+        if os.path.exists(output_path):
+            
+            print(f"Skipping Control Brain region as the plot already exists.")
+            continue
+        
+        BallTrajectories = Config.load_datasets_for_brain_region(brain_region, data_path, registries, downsample_factor=10)
+
+
+        nicknames = region_data['Nickname'].unique()
+        
+        # Plot the Control brain region
+        try:
+            control_nicknames = registries["control_nicknames"]
+            
+            Config.create_control_plot(region_data, control_nicknames, output_path)
+            print("Processed Control Brain region")
+        except Exception as e:
+            print(f"Error processing Control Brain region: {e}")
 
 # Process Miscellaneous brain region
 if miscellaneous_nicknames:
     try:
-        output_path = "/mnt/upramdya_data/MD/MultiMazeRecorder/Plots/250120_coordinates/Full_euclidean_distance_coordinates_line_Miscellaneous.png"
-        Config.create_and_save_plot(region_data, miscellaneous_nicknames, "Miscellaneous", output_path, registries, show_progress=True)
+        output_path = output_dir / "Full_euclidean_distance_coordinates_line_Miscellaneous.png"
+        Config.create_and_save_plot(region_data, miscellaneous_nicknames, "Miscellaneous", output_path, registries, show_signif=True)
         print("Processed Miscellaneous Brain region")
     except Exception as e:
         print(f"Error processing Miscellaneous Brain region: {e}")
-
-# Plot the Control brain region
-try:
-    control_nicknames = registries["control_nicknames"]
-    output_path = "/mnt/upramdya_data/MD/MultiMazeRecorder/Plots/250120_coordinates/Full_euclidean_distance_coordinates_line_Control.png"
-    Config.create_control_plot(region_data, control_nicknames, output_path)
-    print("Processed Control Brain region")
-except Exception as e:
-    print(f"Error processing Control Brain region: {e}")
